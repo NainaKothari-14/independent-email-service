914667b966d085d482d82a6b30eac25175384818
NainaKothari-14 <nainavasai@gmail.com>
2026-01-23 12:22:23 +0530

    Update frontend API base url

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-  },
+  }, 
 });
 
 transporter.verify()
--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+    })
+  );
 
 app.use("/api/email", emailRoutes);
 
